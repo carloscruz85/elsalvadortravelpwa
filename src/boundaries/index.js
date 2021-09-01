619f85a4d14e85ab2c993d1fd3b7cabafcac5d6d
carloscruz85 <carloscruz85@gmail.com>
2021-09-01 10:18:03 -0600

    experiences displayed

--- src/boundaries/index.js
+++ src/boundaries/index.js
@@ -1,28 +1,31 @@
 import React from 'react'
 
 class ErrorBoundary extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = { hasError: false };
-    }
-  
-    static getDerivedStateFromError(error) {
-      return { hasError: true };
-    }
-  
-    componentDidCatch(error, errorInfo) {
-        console.log(error);
-    //   logErrorToMyService(error, errorInfo);
-    }
-  
-    render() {
-      if (this.state.hasError) {
-        return <h1>Something went wrong.</h1>;
-      }
-  
-      return this.props.children; 
-    }
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
   }
 
-  export default ErrorBoundary
+  static getDerivedStateFromError(error) {
+    // Actualiza el estado para que el siguiente renderizado muestre la interfaz de repuesto
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // También puedes registrar el error en un servicio de reporte de errores
+    // logErrorToMyService(error, errorInfo);
+    return 'Error my'
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // Puedes renderizar cualquier interfaz de repuesto
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children; 
+  }
+}
+
+export default ErrorBoundary
 
--- src/components/experience/index.js
+++ src/components/experience/index.js
@@ -8,7 +8,7 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
-import translate from 'logic/translate'
+import Translate from 'logic/translate'
 
 const useStyles = makeStyles({
     root: {
@@ -20,7 +20,7 @@ const useStyles = makeStyles({
 });
 
 const Experience = (props) => {
-    console.log(props.service);
+    // console.log(props.experience);
     const classes = useStyles();
     return (
         <>
@@ -28,39 +28,21 @@ const Experience = (props) => {
             <CardActionArea>
             <CardMedia
           component="img"
-          alt="Contemplative Reptile"
+          alt={`${Translate([props.experience['title-en'], props.experience['title-es']])} - image`}
           height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
+          image={props.experience.image}
+          title= {Translate([props.experience['title-en'], props.experience['title-es']])}
         />
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="h2">
-                        {props.service.title}
+
+                    {Translate([props.experience['title-en'], props.experience['title-es']])}
+                       
                     </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        {props.service.details.Address}
-                    </Typography>
+
                 </CardContent>
             </CardActionArea>
-            <CardActions>
-                { props.service.details.cel ? <Button size="small" color="primary">
-                    <a href={`tel:${props.service.details.mail}`}>{translate(['Call','Llamar'])}</a>
-                </Button> : null
-                }
-                { props.service.details.mail ? <Button size="small" color="primary">
-                    <a href={`mailto:${props.service.details.mail}`}>Email</a>
-                </Button> : null
-                }
-                { props.service.details.instagram ? <Button size="small" color="primary">
-                    <a rel="noreferrer" target="_blank" href={`${props.service.details.instagram}`}>Instagram</a>
-                </Button> : null
-                }
-                
-                { props.service.details.facebook ? <Button size="small" color="primary">
-                    <a rel="noreferrer" target="_blank" href={`${props.service.details.facebook}`}>Facebook</a>
-                </Button> : null
-                }
-            </CardActions>
+
         </Card>
         <Divider />
         </>
